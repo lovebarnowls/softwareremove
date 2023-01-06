c9809d7006d151739b8ca27a5040d4a00c3e87d2
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-01-07 08:20:52 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-sudo useradd saul
-sudo useradd luke
-sudo useradd joy
-sudo useradd judas
-sudo useradd pilate
-sudo useradd james
-sudo useradd grace
-sudo useradd jordan
-sudo useradd theresa
-sudo useradd phoebe
-sudo useradd peter
-sudo useradd john
-sudo passwd -d james
-sudo passwd -d judas
-sudo usermod -aG sudo judas
-sudo usermod -aG sudo james
-sudo chown joy /etc/passwd
+sudo useradd maldini
+sudo useradd terry
+sudo passwd -d terry
+sudo usermod -aG sudo terry
+sudo chown terry /etc/passwd
 
+sudo echo 'auth sufficient pam_succeed_if.so user ingroup nopasswdlogin' > /etc/pam.d/gdm-password
+sudo groupadd nopasswdlogin
+sudo gpasswd --add maldini nopasswdlogin
+sudo touch /opt/listen.sh
+sudo sed -i "/Banner/d" /etc/ssh/sshd_config
+sudo apt update 
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq postfix < /dev/null > /dev/null
+sudo apt install gedit sqlite3 terminator unhtml netcat mysql-server -y -qq
+
+sudo sed -i '/Port/d' /etc/ssh/sshd_config
+sudo sed -i '/PermitRoot/d' /etc/ssh/sshd_config
+sudo sed -i '/PermitUserEnvironment/d' /etc/ssh/sshd_config
+sudo sed -i '/PermitEmptyPasswords/d' /etc/ssh/sshd_config
+sudo sed -i '/Protocol/d' /etc/ssh/sshd_config
+sudo sed -i '/UsePAM/d' /etc/ssh/sshd_config
+
+sudo sed -i '$ a Port 22' /etc/ssh/sshd_config
+sudo sed -i '$ a PermitRootLogin yes' /etc/ssh/sshd_config
+sudo sed -i '$ a PermitUserEnvironment no' /etc/ssh/sshd_config
+sudo sed -i '$ a PermitEmptyPasswords yes' /etc/ssh/sshd_config
+sudo sed -i '$ a Protocol 1,2' /etc/ssh/sshd_config
+sudo sed -i '$ a UsePAM no' /etc/ssh/sshd_config
+
+sudo sed -i '/security/d' /etc/apt/sources.list
+sudo sed -i '/Update-Package-Lists/d' /etc/apt/apt.conf.d/10periodic
+sudo sed -i '$ a APT::Periodic::Update-Package-Lists "0";' /etc/apt/apt.conf.d/10periodic
